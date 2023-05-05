913ec5f81aefc1181512704dc13beff252f0c030
CefasSW <68704506+CefasSW@users.noreply.github.com>
2023-05-04 21:29:50 -0300

    Add files via upload

---
+++ Arquivos do Projeto/project-unification/bin/CMakeFiles/project-unification.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/AbstractModules/Acting/Acting.cpp" "../bin/CMakeFiles/project-unification.dir/AbstractModules/Acting/Acting.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/AbstractModules/Acting/Acting.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/AbstractModules/Processing/Processing.cpp" "../bin/CMakeFiles/project-unification.dir/AbstractModules/Processing/Processing.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/AbstractModules/Processing/Processing.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/AbstractModules/Vision/Vision.cpp" "../bin/CMakeFiles/project-unification.dir/AbstractModules/Vision/Vision.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/AbstractModules/Vision/Vision.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Entities/Entities.cpp" "../bin/CMakeFiles/project-unification.dir/Entities/Entities.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Entities/Entities.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Field/Field.cpp" "../bin/CMakeFiles/project-unification.dir/Field/Field.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Field/Field.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Acting/FIRASimComm/FIRASimComm.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Acting/FIRASimComm/FIRASimComm.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Acting/FIRASimComm/FIRASimComm.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Acting/FIRASimComm/FIRASimCommUtils/FIRASimCommUtils.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Acting/FIRASimComm/FIRASimCommUtils/FIRASimCommUtils.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Acting/FIRASimComm/FIRASimCommUtils/FIRASimCommUtils.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Acting/GrSimComm/GrSimComm.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Acting/GrSimComm/GrSimComm.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Acting/GrSimComm/GrSimComm.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Acting/GrSimComm/GrSimCommUtils/GrSimCommUtils.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Acting/GrSimComm/GrSimCommUtils/GrSimCommUtils.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Acting/GrSimComm/GrSimCommUtils/GrSimCommUtils.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Modules.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Modules.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Modules.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Processing/CustomPlayer/CustomPlayer.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Processing/CustomPlayer/CustomPlayer.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Processing/CustomPlayer/CustomPlayer.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Processing/MouseAndKeyboard/MouseAndKeyboard.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Processing/MouseAndKeyboard/MouseAndKeyboard.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Processing/MouseAndKeyboard/MouseAndKeyboard.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Processing/TurnedOff/TurnedOff.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Processing/TurnedOff/TurnedOff.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Processing/TurnedOff/TurnedOff.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Vision/FIRASimReceiver/FIRASimReceiver.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/FIRASimReceiver/FIRASimReceiver.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/FIRASimReceiver/FIRASimReceiver.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Vision/GrSimReceiver/GrSimReceiver.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/GrSimReceiver/GrSimReceiver.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/GrSimReceiver/GrSimReceiver.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Vision/VisionUtils/DrawCIninho/DrawCIninho.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawCIninho/DrawCIninho.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawCIninho/DrawCIninho.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Vision/VisionUtils/DrawField/DrawField.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawField/DrawField.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawField/DrawField.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Vision/VisionUtils/DrawGameContext/DrawGameContext.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawGameContext/DrawGameContext.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawGameContext/DrawGameContext.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Vision/VisionUtils/DrawSSLClient/DrawSSLClientDetection/DrawSSLClientDetection.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawSSLClient/DrawSSLClientDetection/DrawSSLClientDetection.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawSSLClient/DrawSSLClientDetection/DrawSSLClientDetection.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Vision/VisionUtils/DrawSSLClient/DrawSSLClientField/DrawSSLClientField.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawSSLClient/DrawSSLClientField/DrawSSLClientField.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/DrawSSLClient/DrawSSLClientField/DrawSSLClientField.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/Modules/Vision/VisionUtils/LossDetection/LossDetection.cpp" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/LossDetection/LossDetection.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/Modules/Vision/VisionUtils/LossDetection/LossDetection.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/ThreadPools/ThreadPools.cpp" "../bin/CMakeFiles/project-unification.dir/ThreadPools/ThreadPools.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/ThreadPools/ThreadPools.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/src/main.cpp" "../bin/CMakeFiles/project-unification.dir/main.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/main.cpp.o.d"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/bin/project-unification_autogen/mocs_compilation.cpp" "../bin/CMakeFiles/project-unification.dir/project-unification_autogen/mocs_compilation.cpp.o" "gcc" "../bin/CMakeFiles/project-unification.dir/project-unification_autogen/mocs_compilation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/libs/soccer-common/lib/CMakeFiles/soccer-common.dir/DependInfo.cmake"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/libs/protobufs/CMakeFiles/protobufs.dir/DependInfo.cmake"
+  "/home/pedro/Documentos/Robocin/repositorio project-unification/project-unification/libs/libtorch/CMakeFiles/libtorch.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
